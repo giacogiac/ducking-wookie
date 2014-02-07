c63979915c422f5bd86c675f11c382747411b29d
Giacomo ROMBAUT <rombaut@polytech.unice.fr>
2014-02-07 10:58:19 +0100

    Central generation cours fini

--- POC/src/poc/Central.java
+++ POC/src/poc/Central.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Vector;
 
 public class Central {
@@ -18,6 +19,7 @@ public class Central {
 	private static Map<String, List<CoursBoursier>> bourse = new HashMap<String, List<CoursBoursier>>();
 
 	public static void main(String[] args) {
+		genererCours(CoursBoursier.parseCSV("cours.csv"));
 		ServerSocket ss;
 		try {
 			ss = new ServerSocket(BOURSE_PORT);
@@ -55,9 +57,44 @@ public class Central {
 		
 	}
 	
-	private static void genererCours() {
-		
+	private static void genererCours(List<CoursBoursier> init) {
+		for (CoursBoursier coursBoursier : init) {
+			List<CoursBoursier> historique = new Vector<CoursBoursier>();
+			bourse.put(coursBoursier.ISIN, historique);
+			BourseSimulator simulator = new BourseSimulator(historique, coursBoursier.ISIN, coursBoursier.company, coursBoursier.cotation);
+			new Thread(simulator).start();
+		}
 	}
 	
+	private static class BourseSimulator implements Runnable {
+		private Random rand;
+		private double tendance;
+		
+		private List<CoursBoursier> historique;
+		private double currentValue;	
+		
+		private String ISIN;
+		private String company;
+
+		private BourseSimulator(List<CoursBoursier> historique, String iSIN, String company, double init) {
+			this.historique = historique;
+			this.ISIN = iSIN;
+			this.company = company;
+			currentValue = init;
+			rand = new Random();
+			tendance = rand.nextDouble();
+		}
+
+		@Override
+		public void run() {
+			historique.add(new CoursBoursier(Calendar.getInstance().getTimeInMillis(), ISIN, company, currentValue));
+			while (true) {
+				currentValue += rand.nextDouble() - tendance;
+				tendance += (rand.nextDouble() - 0.5) * 0.1;
+				historique.add(new CoursBoursier(Calendar.getInstance().getTimeInMillis(), ISIN, company, currentValue));
+			}
+		}
+		
+	}
 
 }
--- POC/src/poc/CoursBoursier.java
+++ POC/src/poc/CoursBoursier.java
@@ -1,6 +1,13 @@
 package poc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoursBoursier implements Serializable {
 	/**
@@ -27,4 +34,43 @@ public class CoursBoursier implements Serializable {
 				+ company + ", cotation=" + cotation + "]";
 	}
 
+	public static List<CoursBoursier> parseCSV(String file) {
+
+		ArrayList<CoursBoursier> coursBoursiers = new ArrayList<CoursBoursier>();
+
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(new File(file));
+
+			BufferedReader buffer = new BufferedReader(fileReader);
+			String line = null;
+			do {
+				try {
+					line = buffer.readLine();
+
+					if (null != line) {
+						String[] data = line.split(";");
+
+						coursBoursiers.add(new CoursBoursier(System
+								.currentTimeMillis(), data[0].trim(), data[1]
+								.trim(), Double.parseDouble(data[2])));
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} while (null != line);
+			buffer.close();
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return coursBoursiers;
+	}
+	
 }
