f6af5ca986a7f696913dee20fbdee19ebf4ad6c1
GaspardP <gaspardp@kth.se>
2014-02-07 12:09:30 +0100

    Comparable CoursBoursier

--- POC/src/poc/CoursBoursier.java
+++ POC/src/poc/CoursBoursier.java
@@ -9,68 +9,86 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CoursBoursier implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2670013256597336386L;
+public class CoursBoursier implements Serializable, Comparable<CoursBoursier> {
 
-	public long time;
-	public String ISIN;
-	public String company;
-	public double cotation;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2670013256597336386L;
 
-	public CoursBoursier(long time, String iSIN, String company, double cotation) {
-		super();
-		this.time = time;
-		ISIN = iSIN;
-		this.company = company;
-		this.cotation = cotation;
-	}
+    public long time;
+    public String ISIN;
+    public String company;
+    public double cotation;
 
-	@Override
-	public String toString() {
-		return "CoursBoursier [time=" + time + ", ISIN=" + ISIN + ", company="
-				+ company + ", cotation=" + cotation + "]";
-	}
+    public CoursBoursier(long time, String iSIN, String company, double cotation) {
+        super();
+        this.time = time;
+        ISIN = iSIN;
+        this.company = company;
+        this.cotation = cotation;
+    }
 
-	public static List<CoursBoursier> parseCSV(String file) {
+    @Override
+    public String toString() {
+        return "CoursBoursier [time=" + time + ", ISIN=" + ISIN + ", company="
+                + company + ", cotation=" + cotation + "]";
+    }
 
-		ArrayList<CoursBoursier> coursBoursiers = new ArrayList<CoursBoursier>();
+    public static List<CoursBoursier> parseCSV(String file) {
 
-		FileReader fileReader;
-		try {
-			fileReader = new FileReader(new File(file));
+        ArrayList<CoursBoursier> coursBoursiers = new ArrayList<CoursBoursier>();
 
-			BufferedReader buffer = new BufferedReader(fileReader);
-			String line = null;
-			do {
-				try {
-					line = buffer.readLine();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(new File(file));
 
-					if (null != line) {
-						String[] data = line.split(";");
+            BufferedReader buffer = new BufferedReader(fileReader);
+            String line = null;
+            do {
+                try {
+                    line = buffer.readLine();
 
-						coursBoursiers.add(new CoursBoursier(System
-								.currentTimeMillis(), data[0].trim(), data[1]
-								.trim(), Double.parseDouble(data[2])));
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} while (null != line);
-			buffer.close();
-			fileReader.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+                    if (null != line) {
+                        String[] data = line.split(";");
 
-		return coursBoursiers;
-	}
-	
+                        coursBoursiers.add(new CoursBoursier(System
+                                .currentTimeMillis(), data[0].trim(), data[1]
+                                .trim(), Double.parseDouble(data[2])));
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } while (null != line);
+            buffer.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return coursBoursiers;
+    }
+
+    /**
+     * Natural ordering des cours boursiers
+     * @param cours Autre cours à comparer
+     * @return -1;0;1 si less;equ;grt
+     */
+    @Override
+    public int compareTo(CoursBoursier cours) {
+        assert ISIN.equals(cours.ISIN);
+        
+        if(time == cours.time) {
+            return 0;
+        } else if(time < cours.time) {
+            return -1;
+        }
+        return 1;
+    }
+
 }
