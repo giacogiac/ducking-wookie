584c02cf5536208f609f0a3e396d5d334df8db64
Giacomo ROMBAUT <rombaut@polytech.unice.fr>
2014-02-07 10:07:36 +0100

    Constructeur CoursBoursier

--- POC/src/poc/CoursBoursier.java
+++ POC/src/poc/CoursBoursier.java
@@ -5,4 +5,9 @@ import java.io.Serializable;
 public class CoursBoursier implements Serializable {
 	public long time;
 	public double valeur;
+	
+	public CoursBoursier(long time, double valeur) {
+		this.time = time;
+		this.valeur = valeur;
+	}
 }
