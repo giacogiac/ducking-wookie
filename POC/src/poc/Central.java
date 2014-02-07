64cd2f10358bb7361b47e33587e526ba2c4004f6
Giacomo ROMBAUT <rombaut@polytech.unice.fr>
2014-02-07 09:13:30 +0100

    INIT

---
+++ POC/src/poc/Central.java
@@ -0,0 +1,52 @@
+package poc;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Central {
+	
+	private static final int BOURSE_PORT = 5555;
+
+	public static void main(String[] args) {
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(BOURSE_PORT);
+		} catch (IOException iox) {
+			System.out.println("I/O error at server socket creation");
+			iox.printStackTrace();
+			return;
+		}
+		while (true) {
+			Socket s = null;
+			try {
+				s = ss.accept();
+				System.out.println("connection from" + s.getInetAddress());
+				RegionalHandler handler = new RegionalHandler(s);
+				new Thread(handler).start();
+			} catch (IOException iox) {
+				iox.printStackTrace();
+			}
+		}
+	}
+	
+	private static class RegionalHandler implements Runnable {
+		private ObjectOutputStream toClient;
+		private ObjectInputStream fromClient;
+
+		private RegionalHandler(Socket socket) throws IOException {
+			fromClient = new ObjectInputStream(socket.getInputStream());
+			toClient = new ObjectOutputStream(socket.getOutputStream());
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+}
